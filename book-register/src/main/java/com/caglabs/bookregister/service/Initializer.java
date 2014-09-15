560c0efeceec62a99cf409d1b753c9acab17cc00
Joel Binnquist <joel.binnquist@gmail.com>
2014-09-16 00:18:50 +0200

    lab 3 even more

--- book-register/src/main/java/com/caglabs/bookregister/service/Initializer.java
+++ book-register/src/main/java/com/caglabs/bookregister/service/Initializer.java
@@ -43,29 +43,53 @@ public class Initializer {
                 "Newton",
                 new GregorianCalendar(1642, 11, 25).getTime(),
                 Lists.<Book>newArrayList());
+        Author author3 = new Author(
+                "Douglas",
+                "Adams",
+                new GregorianCalendar(1952, 3, 11).getTime(),
+                Lists.<Book>newArrayList());
         Book book1 = new Book(
                 "The Grand Design",
                 newArrayList(author1),
                 "0-553-80537-1",
                 new GregorianCalendar(2010, 7, 7).getTime());
         book1.authors.add(author1);
+        author1.publications.add(book1);
         Book book2 = new Book(
                 "Black Holes and Baby Universes and Other Essays",
                 newArrayList(author1),
                 "0307790452",
                 new GregorianCalendar(2011, 0, 1).getTime());
         book2.authors.add(author1);
+        author1.publications.add(book2);
         Book book3 = new Book(
                 "Philosophiae Naturalis Principia Mathematica",
                 newArrayList(author2),
                 "9780762420223",
                 new GregorianCalendar(1687, 0, 1).getTime());
-
+        author2.publications.add(book3);
+        Book book4 = new Book(
+                "The Hitchiker's Guide to the Galaxy",
+                newArrayList(author3),
+                "0-330-25864-8",
+                new GregorianCalendar(1979, 10, 12).getTime());
+        author3.publications.add(book4);
+        Book book5 = new Book(
+                "Fictive",
+                newArrayList(author1,author2,author3),
+                "42",
+                new GregorianCalendar(2145, 10, 12).getTime());
+        author1.publications.add(book5);
+        author2.publications.add(book5);
+        author3.publications.add(book5);
         entityManager.persist(author1);
         entityManager.persist(author2);
+        entityManager.persist(author3);
         entityManager.persist(book1);
         entityManager.persist(book2);
         entityManager.persist(book3);
+        entityManager.persist(book4);
+        entityManager.persist(book5);
         entityManager.flush();
         System.out.printf("Initialization complete!!!!\n");
     }
