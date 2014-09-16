ef91e5c7b4f15d6f960998a613cfdf2fa596f36b
Joel Binnquist <joel.binnquist@gmail.com>
2014-09-16 20:45:30 +0200

    Fix error in authors relation on server and clean up controllers in lab2-finals

--- book-register/src/main/java/com/caglabs/bookregister/domain/Book.java
+++ book-register/src/main/java/com/caglabs/bookregister/domain/Book.java
@@ -11,7 +11,7 @@ import java.util.List;
 @Entity
 public class Book extends AbstractEntity {
     public String title;
-    @ManyToMany(cascade = {CascadeType.DETACH, CascadeType.MERGE, CascadeType.PERSIST, CascadeType.REFRESH})
+    @ManyToMany(cascade = {CascadeType.DETACH, CascadeType.MERGE, CascadeType.PERSIST, CascadeType.REFRESH}, mappedBy = "publications")
     public List<Author> authors;
     public String isbn;
     @Temporal(TemporalType.DATE)
--- lab2-final/src/miniApp.js
+++ lab2-final/src/miniApp.js
@@ -1,58 +1,38 @@
 (function () {
   function handleError(data, status, headers, config) {
-    console.log('data: %o, status: %o, headers: %o, config: %o', data, status, headers(), config);
+    console.log('data: %o, status: %o, headers: %o, config: %o', data, status, headers && headers(), config);
     alert('Schmäll!!!\nFelkod: ' + status + ', felinfo: "' + data + '"');
   }
 
   angular.module('miniApp', ['ngRoute'])
-
-    .controller('MainController', function ($scope, $route, $routeParams, $location) {
-      $scope.$route = $route;
-      $scope.$location = $location;
-      $scope.$routeParams = $routeParams;
-      $scope.$on('$routeChangeSuccess', function (event, toState, fromState) {
-        console.log('event', event);
-        console.log('toState', toState);
-        console.log('fromState', fromState);
-      })
-    })
-
-    .controller('BooksCtrl', function ($scope, $http, $q) {
-      $q.all(
+    .controller('BooksCtrl', function ($scope, $http) {
         $http.get('http://localhost:8080/books')
           .success(function (data, status, headers, config) {
             console.log('data: %o, status: %o, headers: %o, config: %o', data, status, headers && headers(), config);
             $scope.books = data._embedded ? data._embedded.books : [];
           })
-          .error(handleError),
-        $http.get('http://localhost:8080/authors')
-          .success(function (data, status, headers, config) {
-            console.log('data: %o, status: %o, headers: %o, config: %o', data, status, headers(), config);
-            $scope.authors = data._embedded ? data._embedded.authors : [];
-          })
-          .error(handleError));
+          .error(handleError);
 
       $scope.select = function (book) {
         $scope.selectedBook = book;
-        //if ($scope.selectedBook.authors === undefined) {
-        //  $http.get(book._links.authors.href).success(function(data){
-        //    var authors = data._embedded.authors || [];
-        //    $scope.selectedBook.authors = [];
-        //    authors.forEach(function(author){
-        //      $scope.selectedBook.authors.push(author);
-        //    });
-        //  }).error(handleError());
-        //}
+        if ($scope.selectedBook.authors === undefined) {
+          $http.get(book._links.authors.href).success(function (data) {
+            var authors = data._embedded.authors || [];
+            $scope.selectedBook.authors = [];
+            authors.forEach(function (author) {
+              $scope.selectedBook.authors.push(author);
+            });
+          }).error(handleError);
+        }
       }
-    }
-  )
-
-    .
-    controller('AuthorsCtrl', function ($scope, $http) {
+    })
+    .controller('AuthorsCtrl', function ($scope, $http) {
       $http.get('http://localhost:8080/authors')
         .success(function (data, status, headers, config) {
-          console.log('data: %o, status: %o, headers: %o, config: %o', data, status, headers(), config);
-          $scope.authors = (data._embedded ? data._embedded.authors : []).sort(function(a,b){return a.surname > b.surname;});
+          console.log('data: %o, status: %o, headers: %o, config: %o', data, status, headers && headers(), config);
+          $scope.authors = (data._embedded ? data._embedded.authors : []).sort(function (a, b) {
+            return a.surname > b.surname;
+          });
         })
         .error(handleError);
 
